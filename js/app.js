80cd1b231563a3448d9e149b58188a35f90fca31
Lucjan Wiśnicz <luqapo@gmail.com>
2018-08-15 21:47:17 +0200

    versja 1.0

---
+++ js/app.js
@@ -0,0 +1,127 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    function GameOfLife(boardWidth,boardHeight){
+        this.width = boardWidth;
+        this.height = boardHeight;
+        this.cells = [];
+
+        this.crateBoard = function(){
+            this.board = document.getElementById("board");
+            this.board.style.width = `${this.width*10}px`;
+            this.board.style.height = `${this.height*10}px`;
+            this.divsIndex = this.width * this.height;
+
+            for(var i = 0; i< this.divsIndex; i++){
+                var div = document.createElement("div");
+                this.board.appendChild(div);
+            }
+            this.cells = document.querySelectorAll("#board div");
+            this.cells.forEach(function(a){
+                return a.addEventListener("mouseenter",function(){
+                    this.classList.toggle("live");
+                })
+            })
+        }
+
+        this.cellIndex = function(x,y){
+           return this.cells[x + (y * this.width)];
+        }
+
+        this.setCellState = function(x,y,state){
+            if(this.cellIndex(x,y).className != state) {
+                this.cellIndex(x, y).classList.add("live")
+            }
+        }
+
+        this.computeCellNextState = function(x,y){
+            var liveIndex = 0;
+            if( x > 0 && y > 0 && this.cellIndex(x-1,y-1).className == "live"){
+                liveIndex++;
+            } if(y > 0 && this.cellIndex(x,y-1).className == "live"){
+                liveIndex++;
+            } if(y > 0 && x < this.width-1 && this.cellIndex(x+1,y-1).className == "live"){
+                liveIndex++;
+            } if(x > 0 && this.cellIndex(x-1,y).className == "live"){
+                liveIndex++;
+            } if(x < this.width-1 && this.cellIndex(x+1,y).className == "live"){
+                liveIndex++;
+            } if(x> 0 && y < this.height-1 && this.cellIndex(x-1,y+1).className == "live"){
+                liveIndex++;
+            } if(y < this.height-1 && this.cellIndex(x,y+1).className == "live"){
+                liveIndex++;
+            } if(x < this.width-1 && y < this.height-1 && this.cellIndex(x+1,y+1).className == "live"){
+                liveIndex++;
+            } if (liveIndex == 2 && this.cellIndex(x,y).className == 'live'){
+                return 1;
+            } if (liveIndex == 3 && this.cellIndex(x,y).className == 'live'){
+            return 1;
+            } if (liveIndex == 3 && this.cellIndex(x,y).className != 'live'){
+            return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        this.computeNextGeneration = function(){
+            this.nextBoard = [];
+            for(var i = 0; i<this.height; i++){
+                for(var j = 0; j<this.width; j++){
+                    this.nextBoard.push(this.computeCellNextState(j,i));
+                }
+            }
+        }
+
+        this.printNextGeneration = function(){
+            for(var i = 0; i<this.cells.length; i++){
+                if(this.nextBoard[i] == 1 && this.cells[i].className != 'live'){
+                    this.cells[i].classList.add("live");
+                } else if (this.nextBoard[i] == 0 && this.cells[i].className == "live"){
+                    this.cells[i].classList.remove("live");
+                }
+            }
+        }
+
+        this.playGame = function(){
+            var self = this;
+            this.pauseId = setInterval(function(){
+                self.computeNextGeneration();
+                self.printNextGeneration();
+            }, 500);
+        }
+
+        this.startGame = function(){
+
+            game.crateBoard();
+
+            var play = document.getElementById("play");
+            var pause = document.getElementById("pause");
+
+            play.addEventListener("click",function(){
+                game.playGame();
+            })
+
+            pause.addEventListener("click",function () {
+                clearInterval(game.pauseId);
+            })
+        }
+    }
+
+    var startGame = document.getElementById("startGame");
+    var boardWidth = 0;
+    var boardHeight = 0;
+    var game = {};
+
+    startGame.addEventListener("click",function(){
+        boardWidth = document.getElementById("width").value;
+        boardHeight = document.getElementById("height").value;
+        document.getElementById("start").style.display = "none";
+        document.getElementById("game-set").style.display = "block";
+
+        game = new GameOfLife(boardWidth,boardHeight);
+        game.startGame();
+    })
+
+
+
+
+})
